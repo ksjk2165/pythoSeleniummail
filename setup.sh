db7fe6207a3c2d5c5ccc0164de91a7bdb14de16a
ksjk2165 <76752629+ksjk2165@users.noreply.github.com>
2021-03-26 01:08:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
-pip3 install selenium
+
+
+apt install screen
+apt install python3
 apt-get update # to update ubuntu to correctly run apt install
 apt install chromium-chromedriver
 cp /usr/lib/chromium-browser/chromedriver /usr/bin
+pip3 install selenium
+
+
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
 
